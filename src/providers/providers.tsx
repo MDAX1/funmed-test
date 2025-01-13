1801c0d91a671f1f9804b5470f0ea7cdc4c4347b
Mehrdad <amini_pk@hotmail.com>
2025-01-13 18:46:23 +0100

    feat: Updated Providers Component

--- src/providers/providers.tsx
+++ src/providers/providers.tsx
@@ -1,14 +1,17 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { SearchProvider } from '../contexts/SearchContext';
+import { ThemeProvider } from '../contexts/ThemeContext';
 
 const queryClient = new QueryClient();
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <QueryClientProvider client={queryClient}>
-      <SearchProvider>
-        {children}
-      </SearchProvider>
+      <ThemeProvider>
+        <SearchProvider>
+          {children}
+        </SearchProvider>
+      </ThemeProvider>
     </QueryClientProvider>
   );
 }
